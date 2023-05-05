d1edb109cede15151413be53cd28d14d0069abb4
yuriy-aza <yuriy.aza@gmail.com>
2023-05-05 09:39:01 +0300

    Project create

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './js/api';
+import './js/authorization';
+import './js/books-all';
+import './js/books-genre';
+import './js/categories-list';
+import './js/header';
+import './js/modal-window';
+import './js/shopping-list';
+import './js/support-ukraine';
