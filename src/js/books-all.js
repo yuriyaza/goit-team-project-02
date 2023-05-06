5a59e9eee2ade6a4b5e8b50cc819f5d97d35ca24
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-05-07 00:07:33 +0300

    1

--- src/js/books-all.js
+++ src/js/books-all.js
@@ -1,24 +1,24 @@
-const btn = document.querySelector('.see-more');
-const cards = Array.from(document.querySelectorAll('.books-all-item'))
+// const btn = document.querySelector('.see-more');
+// const cards = Array.from(document.querySelectorAll('.books-all-item'))
 
-function openCatalog() {
-    btn.addEventListener('click', () => {
-        cards.forEach(item => item.classList.remove('hidden'));
+// function openCatalog() {
+//     btn.addEventListener('click', () => {
+//         cards.forEach(item => item.classList.remove('hidden'));
         
-    })
-}
+//     })
+// }
 
-function response1() {
-    if(window.innerWidth > 1439) {
-        cards.forEach((item, index) =>{
-            item.classList.add('hidden')
-            if (index <= 4) {
-                item.classList.remove('hidden')
-            } 
-            openCatalog()
-        })
-    }
-}
+// function response1() {
+//     if(window.innerWidth > 1439) {
+//         cards.forEach((item, index) =>{
+//             item.classList.add('hidden')
+//             if (index <= 4) {
+//                 item.classList.remove('hidden')
+//             } 
+//             openCatalog()
+//         })
+//     }
+// }
 
 
 // ===== Отримання даних із сервера та створення динамічної розмітки =====
@@ -48,12 +48,9 @@ function booksAllCreateMarkup(backEndCategories) {
 
 function booksAllCreateOneCategory(bookCategory) {
   return `
+    <h3 class="books-all-menu">${bookCategory.list_name}</h3>
     <ul class="books-all">
       <li class="books-all-item">
-        <h3 class="books-all-menu">${bookCategory.list_name}</h3>
-        <ul>
-          <li>
-            <a class="books-all-link" href="#" data-modal-open data-id=" ">
             <img class="books-all-image" src="${bookCategory.books[0].book_image}" alt="${bookCategory.books[0].title}" loading="lazy">
             <div class="books-info">
               <p class="info-item">${bookCategory.books[0].title}</p>
@@ -61,41 +58,33 @@ function booksAllCreateOneCategory(bookCategory) {
             </div>
             </a>
           </li>
-          <li>
-            <a class="books-all-link" href="#" data-modal-open data-id=" ">
+      <li class="books-all-item">
             <img class="books-all-image" src="${bookCategory.books[1].book_image}" alt="${bookCategory.books[1].title}" loading="lazy">
             <div class="books-info">
               <p class="info-item">${bookCategory.books[1].title}</p>
               <p class="info-detail-item">${bookCategory.books[1].author}</p>
             </div>
-            </a>
           </li>
-          <li>
-            <a class="books-all-link" href="#" data-modal-open data-id=" ">
+      <li class="books-all-item">
             <img class="books-all-image" src="${bookCategory.books[2].book_image}" alt="${bookCategory.books[2].title}" loading="lazy">
             <div class="books-info">
               <p class="info-item">${bookCategory.books[2].title}</p>
               <p class="info-detail-item">${bookCategory.books[2].author}</p>
             </div>
-            </a>
           </li>
-          <li>
-            <a class="books-all-link" href="#" data-modal-open data-id=" ">
+        <li class="books-all-item">
             <img class="books-all-image" src="${bookCategory.books[3].book_image}" alt="${bookCategory.books[3].title}" loading="lazy">
             <div class="books-info">
               <p class="info-item">${bookCategory.books[3].title}</p>
               <p class="info-detail-item">${bookCategory.books[3].author}</p>
             </div>
-            </a>
-          </li>
-          <li>
-            <a class="books-all-link" href="#" data-modal-open data-id=" ">
+        </li>
+        <li class="books-all-item">
             <img class="books-all-image" src="${bookCategory.books[4].book_image}" alt="${bookCategory.books[4].title}" loading="lazy">
             <div class="books-info">
               <p class="info-item">${bookCategory.books[4].title}</p>
               <p class="info-detail-item">${bookCategory.books[4].author}</p>
             </div>
-            </a>
           </li>
         </ul>
         <button class="see-more" type="button">SEE MORE</button>
