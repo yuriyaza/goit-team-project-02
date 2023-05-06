8f17d0b4a6f6f7868e26df8fe6feaa18bd97b5c1
Enital <117684114+Enital@users.noreply.github.com>
2023-05-06 15:20:30 +0300

    cat-list

--- src/js/categories-list.js
+++ src/js/categories-list.js
@@ -1,20 +1,22 @@
 import { BookAPI } from "./api";
 // import { ScrollSpy } from "bootstrap";
-import PerfectScrollbar from 'perfect-scrollbar';
+// import PerfectScrollbar from 'perfect-scrollbar';
 
 catListEl = document.querySelector('.category-list');
 const api = new BookAPI;
+const inputEl = document.querySelector('.category-list');
 
-new PerfectScrollbar('#scroll-container');
+(getData());
+// new PerfectScrollbar('#scroll-container');
 
-const scrollEl = document.querySelector('#scroll-container');
+// const scrollEl = document.querySelector('#scroll-container');
 // const ps = new PerfectScrollbar(scrollEl);
 
-const ps = new PerfectScrollbar('#scroll-container', {
-wheelSpeed: 1,
-// wheelPropagation: true,
-// minScrollbarLength: 20
-});
+// const ps = new PerfectScrollbar('#scroll-container', {
+// wheelSpeed: 1,
+// // wheelPropagation: true,
+// // minScrollbarLength: 20
+// });
 
 // document.querySelector('#resize').addEventListener('click', () => {
 //         // Get updated values
@@ -33,25 +35,43 @@ async function getData() {
     console.log('local storage');
     const categoryList = await api.getCategoryList();
     localStorage.setItem("category-list", JSON.stringify(categoryList));
-
+    catListMarkup();
 }
-getData();
+
 
 function catListMarkup() {
-    // const categoryList = await api.getCategoryList();
-    // console.log(categoryList);
     const savedData = localStorage.getItem("category-list");
     const parsedData = JSON.parse(savedData);
     const markup = parsedData.reduce((acc, { list_name }) => {
         return acc += `
         <a href="#">
-        <li class="category-li">
-            ${list_name}
-        </li>
+            <li class="category-li">${list_name}</li>
         </a>
-    `},''
-    )
+    `}, '')
+    console.log(markup);
     catListEl.insertAdjacentHTML('beforeend', markup);
 };
 
-catListMarkup();
+
+inputEl.addEventListener('click', clickFunc);
+
+function clickFunc(event) {
+    if (event.target.nodeName !== 'LI') {
+        return
+    }
+    // console.log(event.target.nodeName)
+    // console.log(event.currentTarget.textContent);
+    getCategoryFunc(event.target.textContent);
+}
+
+function getCategoryFunc(data){
+    
+    // console.log('123', data);
+    const dataWords = data.split(' ');
+    // console.log(dataWords);
+    const opt = dataWords.join('%20');
+    
+    console.log(`https://books-backend.p.goit.global/books/category?category=${opt}`);
+}
+
+
