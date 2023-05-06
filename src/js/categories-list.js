6775c7f92aeb77ceb1fdf7714820cda840aaad3e
Enital <117684114+Enital@users.noreply.github.com>
2023-05-06 18:17:57 +0300

    cat-list-06-1

--- src/js/categories-list.js
+++ src/js/categories-list.js
@@ -2,37 +2,16 @@ import { BookAPI } from "./api";
 // import { ScrollSpy } from "bootstrap";
 // import PerfectScrollbar from 'perfect-scrollbar';
 
-catListEl = document.querySelector('.category-list');
+const catListEl = document.querySelector('.category-list');
 const api = new BookAPI;
-const inputEl = document.querySelector('.category-list');
+// const inputEl = document.querySelector('.category-list');
+let upperCaseNow = 'All categories';
+let upperCase = '';
 
 (getData());
-// new PerfectScrollbar('#scroll-container');
-
-// const scrollEl = document.querySelector('#scroll-container');
-// const ps = new PerfectScrollbar(scrollEl);
-
-// const ps = new PerfectScrollbar('#scroll-container', {
-// wheelSpeed: 1,
-// // wheelPropagation: true,
-// // minScrollbarLength: 20
-// });
-
-// document.querySelector('#resize').addEventListener('click', () => {
-//         // Get updated values
-//         width = document.querySelector('#width').value;
-//         height = document.querySelector('#height').value;
-
-//         // Set demo sizes
-//         demo.style.width = `${width}px`;
-//         demo.style.height = `${height}px`;
-
-//         // Update Perfect Scrollbar
-//         ps.update();
-//     });
 
 async function getData() {
-    console.log('local storage');
+    // console.log('local storage');
     const categoryList = await api.getCategoryList();
     localStorage.setItem("category-list", JSON.stringify(categoryList));
     catListMarkup();
@@ -40,28 +19,44 @@ async function getData() {
 
 
 function catListMarkup() {
-    const savedData = localStorage.getItem("category-list");
-    const parsedData = JSON.parse(savedData);
-    const markup = parsedData.reduce((acc, { list_name }) => {
+    const array = loadFromLocalStorage();
+    const markup = array.reduce((acc, { list_name }) => {
         return acc += `
-        <a href="#">
-            <li class="category-li">${list_name}</li>
-        </a>
+            <li class="category-li">
+                <a class="category" href="#">${list_name}</a>
+            </li>
     `}, '')
-    console.log(markup);
+    // console.log(markup);
     catListEl.insertAdjacentHTML('beforeend', markup);
 };
 
-
-inputEl.addEventListener('click', clickFunc);
+catListEl.addEventListener('click', clickFunc);
 
 function clickFunc(event) {
-    if (event.target.nodeName !== 'LI') {
+    if (event.target.nodeName !== 'A') {
         return
     }
     // console.log(event.target.nodeName)
     // console.log(event.currentTarget.textContent);
-    getCategoryFunc(event.target.textContent);
+    if (event.target.textContent === 'All categories') {
+        removeUpperCase();
+        event.target.classList.add('upper-case');
+        console.log(`Люда     getTopBooks()`);
+    } else {
+        // if (upperCaseNow === 'All categories') {
+        //     removeUpperCase();
+        // }
+        upperCase = upperCaseNow;
+        upperCaseNow = event.target.textContent;
+        // console.log('uppercasenow',upperCaseNow);
+        // console.log(event.target.textContent)
+
+        // makeUpperCase(event.target);
+        // event.target.
+        removeUpperCase();
+        event.target.classList.add('upper-case');
+        getCategoryFunc(event.target.textContent);
+    }
 }
 
 function getCategoryFunc(data){
@@ -71,7 +66,30 @@ function getCategoryFunc(data){
     // console.log(dataWords);
     const opt = dataWords.join('%20');
     
-    console.log(`https://books-backend.p.goit.global/books/category?category=${opt}`);
+    console.log(`Олег      https://books-backend.p.goit.global/books/category?category=${opt}`);
+    // 'https://books-backend.p.goit.global/books/category?category=Combined%20Print%20and%20E-Book%20Nonfiction'
+};
+
+function loadFromLocalStorage() {
+    const savedData = localStorage.getItem("category-list");
+    const parsedData = JSON.parse(savedData);
+    return parsedData;
 }
 
+function makeUpperCase(data) {
+    upperCase = upperCaseNow;
+    upperCaseNow = data.textContent;
+    // console.log('adfgd', category);
+    // console.log('upperCaseNow', upperCaseNow);
+    // const array = loadFromLocalStorage();
+    // const indexUpperCase = array.findIndex(opt => opt.list_name === upperCaseNow);
+    // console.log(indexUpperCase);
+    // data.classList.add('upper-case');
+};
 
+function removeUpperCase() {
+    const rem = document.querySelectorAll('.category');
+    // console.log(rem);
+    // rem[0].classList.remove('upper-case');
+    rem.forEach(el => el.classList.remove('upper-case'))
+}
