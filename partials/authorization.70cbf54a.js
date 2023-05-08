bb3b821cbff15602c5274c51a6ec39f33b441da9
yuriyaza <118635676+yuriyaza@users.noreply.github.com>
2023-05-08 18:48:53 +0000

    Deploying to gh-pages from @ yuriyaza/goit-team-project-02@ec0a42deba34f012ce562af19dd0a7fd98664718 🚀

---
+++ partials/authorization.70cbf54a.js
@@ -0,0 +1,2 @@
+var closePopUp=document.querySelector("#popup-close"),popUp=document.querySelector("#popup");window.location.pathname.includes("partials/authorization.html")&&popUp.classList.add("active"),closePopUp.addEventListener("click",(function(){window.location.href="/",popUp.classList.remove("active")}));
+//# sourceMappingURL=authorization.70cbf54a.js.map
--- partials/authorization.ee4cf2ee.js
+++
@@ -1,2 +0,0 @@
-var closePopUp=document.querySelector("#popup-close"),popUp=document.querySelector("#popup");"/partials/authorization.html"===window.location.pathname&&popUp.classList.add("active"),closePopUp.addEventListener("click",(function(){window.location.href="/",popUp.classList.remove("active")}));
-//# sourceMappingURL=authorization.ee4cf2ee.js.map
