406619af44cf643e548e72f5a7d219fd29a5dec4
EVGENY4510 <lentiay4510z@gmail.com>
2023-05-09 23:46:25 +0300

    fix markup

--- src/js/array-of-funds.js
+++ src/js/array-of-funds.js
@@ -1,57 +1,76 @@
+const save = require('../images/save.png');
+const save2 = require('../images/save@2x.png');
+const hope = require('../images/hope.png');
+const hope2 = require('../images/hope@2x.png');
+const banc = require('../images/24u.png');
+const banc2 = require('../images/24u2x.png');
+const medical = require('../images/medical.png');
+const medical2 = require('../images/medical@2x.png');
+const sans = require('../images/sans.png');
+const sans2 = require('../images/sans@2x.png');
+const razom = require('../images/razom.png');
+const razom2 = require('../images/razom@2x.png');
+const hunger = require('../images/hunger.png');
+const hunger2 = require('../images/hunger@2x.png');
+const vision = require('../images/vision.png');
+const vision2 = require('../images/vision@2x.png');
+const prytula = require('../images/prytula.png');
+const prytula2 = require('../images/prytula@2x.png');
+
 const fonds = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: './images/save.png',
-    img2x: './images/save@2x.png',
+    img: `${save}`,
+    img2x: `${save2}`,
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: './images/hope.png',
-    img2x: './images/hope@2x.png',
+    img: `${hope}`,
+    img2x: `${hope2}`,
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: './images/24u.png',
-    img2x: './images/24u2x.png',
+    img: `${banc}`,
+    img2x: `${banc2}`,
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: './images/medical.png',
-    img2x: './images/medical@2x.png',
+    img: `${medical}`,
+    img2x: `${medical2}`,
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: './images/sans.png',
-    img2x: './images/sans@2x.png',
+    img: `${sans}`,
+    img2x: `${sans2}`,
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: './images/razom.png',
-    img2x: './images/razom@2x.png',
+    img: `${razom}`,
+    img2x: `${razom2}`,
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: './images/hunger.png',
-    img2x: './images/hunger@2x.png',
+    img: `${hunger}`,
+    img2x: `${hunger2}`,
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: './images/vision.png',
-    img2x: './images/vision@2x.png',
+    img: `${vision}`,
+    img2x: `${vision2}`,
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: './images/serhiy.png',
-    img2x: './images/serhiy@2x.png',
+    img: `${prytula}`,
+    img2x: `${prytula2}`,
   },
 ];
 
