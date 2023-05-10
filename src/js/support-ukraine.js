5fcdc1c0790d8bf1e3deda51753af06344da265a
EVGENY4510 <lentiay4510z@gmail.com>
2023-05-10 23:02:08 +0300

    nun

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -1,8 +1,7 @@
 // import { func } from 'joi';
 import { fonds } from './array-of-funds';
-
-const supportList = document.querySelector('.support-ukraine-list');
 let offset = 0;
+const supportList = document.querySelector('.support-ukraine-list');
 const sliderLine = document.querySelector('.slider-liner-su');
 const sliderBtn = document.querySelector('.support-ukraine-btn');
 sliderBtn.addEventListener('click', sliderBtnHandler);
