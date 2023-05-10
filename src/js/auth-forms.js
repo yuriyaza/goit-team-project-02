7f4684a23c65753304654c1398997cd93f333fa6
yuriy-aza <yuriy.aza@gmail.com>
2023-05-10 09:15:16 +0300

    Two auth modal

--- src/js/auth-forms.js
+++ src/js/auth-forms.js
@@ -2,25 +2,63 @@ import scrollLock from 'scroll-lock';
 
 // Открытие 1 модалки
 
-const openPopUp = document.querySelector('.header-sign-btn');
-const closePopUp = document.querySelector('#popup-close');
-const popUp = document.querySelector('#popup');
-const backdrop = document.querySelector('#popup-container');
+const popUp1 = document.querySelector('#popup-1');
+const backdrop1 = document.querySelector('#popup-container-1');
+const openPopUp1 = document.querySelector('.header-sign-btn');
+const closePopUp1 = document.querySelector('#popup-close-1');
+const switchPupUp1 = document.querySelector('#switch-button-1');
 
-openPopUp.addEventListener('click', openAuthPopUp);
-closePopUp.addEventListener('click', closeAuthPopUp);
-backdrop.addEventListener('click', onBackdropClick);
+openPopUp1.addEventListener('click', openAuthPopUp1);
+closePopUp1.addEventListener('click', closeAuthPopUp);
+switchPupUp1.addEventListener('click', switchAuthPopUp1);
+backdrop1.addEventListener('click', onBackdropClick);
 window.addEventListener('keydown', onKeyPress);
 
-function openAuthPopUp(event) {
-  event.preventDefault();
-  popUp.classList.add('active');
+function openAuthPopUp1(event) {
+  try {
+    event.preventDefault();
+  } catch {}
+  popUp1.classList.add('active');
   scrollLock.disablePageScroll(document.body);
   window.addEventListener('keydown', onKeyPress);
 }
 
+function switchAuthPopUp1() {
+  closeAuthPopUp();
+  openAuthPopUp2();
+}
+
+// Открытие 2 модалки
+
+const popUp2 = document.querySelector('#popup-2');
+const backdrop2 = document.querySelector('#popup-container-2');
+const closePopUp2 = document.querySelector('#popup-close-2');
+const switchPupUp2 = document.querySelector('#switch-button-2');
+
+closePopUp2.addEventListener('click', closeAuthPopUp);
+switchPupUp2.addEventListener('click', switchAuthPopUp2);
+backdrop2.addEventListener('click', onBackdropClick);
+window.addEventListener('keydown', onKeyPress);
+
+function openAuthPopUp2(event) {
+  try {
+    event.preventDefault();
+  } catch {}
+  popUp2.classList.add('active');
+  scrollLock.disablePageScroll(document.body);
+  window.addEventListener('keydown', onKeyPress);
+}
+
+function switchAuthPopUp2() {
+  closeAuthPopUp();
+  openAuthPopUp1();
+}
+
+// Общие функции
+
 function closeAuthPopUp() {
-  popUp.classList.remove('active');
+  popUp1.classList.remove('active');
+  popUp2.classList.remove('active');
   scrollLock.enablePageScroll(document.body);
   window.removeEventListener('keydown', onKeyPress);
 }
@@ -36,39 +74,6 @@ function onKeyPress(event) {
     closeAuthPopUp();
   }
 }
-
-// Открытие 2 модалки
-
-const openPopUp2 = document.querySelector('#open-modal-2');
-const closePopUp2 = document.querySelector('#popup-close-2');
-const popUp2 = document.querySelector('#popup-2');
-
-openPopUp2.addEventListener('click', openAuthPopUp2);
-closePopUp2.addEventListener('click', closeAuthPopUp2);
-
-function openAuthPopUp2(event) {
-  event.preventDefault();
-  popUp.classList.add('active');
-}
-
-const closeAuthPopUp2 = () => {
-  popUp.style.display = 'none';
-  popUp2.style.display = 'none';
-};
-
-closePopUp2.forEach(closePopUp2 => {
-  closePopUp2.addEventListener('click', () => {
-    closeAuthPopUp2();
-  });
-});
-
-window.addEventListener('click', event => {
-  if (event.target === popUp || event.target === popUp2) {
-    closeModals();
-  }
-});
-
-
 
 
 // Вариант новый
@@ -121,8 +126,6 @@ window.addEventListener('click', event => {
 //   }
 // }
 
-
-
 // Вариант старый
 
 // if (window.location.pathname.includes('partials/authorization.html')) {
@@ -138,4 +141,3 @@ window.addEventListener('click', event => {
 // window.location.href = 'https://yuriyaza.github.io/goit-team-project-02/';
 // popUp.classList.remove('active');
 // });
-
